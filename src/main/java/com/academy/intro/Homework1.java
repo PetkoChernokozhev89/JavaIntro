bfe5f01c0a151124687bb861bf4e716cc0dad610
PetkoChernokozhev89 <p.chernokozhev89@gmail.com>
2021-01-23 16:24:49 +0200

    Initial commit

---
+++ src/main/java/com/academy/intro/Homework1.java
@@ -0,0 +1,16 @@
+package com.academy.intro;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+   Scanner scanner = new Scanner(System.in);
+   String PetName = scanner.nextLine();
+   String action = scanner.nextLine();
+   System.out.println(PetName + " favourite game is " + action);
+
+
+
+    }
+}
